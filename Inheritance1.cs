c8837b9fb07fd79b10575b5abeaf4c88358e63b0
DENNIS MWANGI <dennistrevor06@gmail.com>
2020-12-07 21:29:56 +0000

    practice

--- Inheritance1.cs
+++ Inheritance1.cs
@@ -1,31 +1,41 @@
 /*
- *  C# Program to Illustrate Single Inheritance
+ *  C# Program to Illustrate Hierarchical Inheritance
  */
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-namespace Inheritance
+namespace Inheritance1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Principal g = new Principal();
+            g.Monitor();
             Teacher d = new Teacher();
+            d.Monitor();
             d.Teach();
             Student s = new Student();
+            s.Monitor();
             s.Learn();
-            s.Teach();
             Console.ReadKey();
         }
-        class Teacher
+        class Principal
+        {
+            public void Monitor()
+            {
+                Console.WriteLine("Monitor");
+            }
+        }
+        class Teacher : Principal
         {
             public void Teach()
             {
                 Console.WriteLine("Teach");
             }
         }
-        class Student : Teacher
+        class Student : Principal
         {
             public void Learn()
             {
